c23cc5cbf0219848c40eadbba78436d72ab312d0
Noelia Rodriguez <nrodriguez@softtekprojects.com>
2025-03-05 22:31:41 -0300

    Diff entre Migrations Code first y Reverse engineering Ingenieria inversa

--- WebApi/Data/PizzaContext.cs
+++ WebApi/Data/PizzaContext.cs
@@ -15,5 +15,23 @@ namespace WebApi.Data
         public DbSet<Sauce> Sauces => Set<Sauce>();
 
         //https://learn.microsoft.com/en-us/training/modules/persist-data-ef-core
+
     }
 }
+
+/*
+ * Manage database schemas
+EF Core provides two primary ways of keeping your EF Core model 
+and database schema in sync:
+
+- Migrations (model as source of truth) Code First -> ContosoPizza.db
+- Reverse engineering (database as source of truth) -> Promotions.db
+
+To choose between these options, decide whether your EF Core model 
+or the database schema is the source of truth.
+
+Migrations o Code First, crea la bd a partir del modelo
+Reverse engineering, crea el modelo a partir de una bd ya existente
+
+
+*/
